52c0fdc82337919e4724dbdbbdd111524d1bc10d
DEVANAND GIRI <102689281+gdev8584@users.noreply.github.com>
2022-10-13 19:40:21 +0530

    Initial commit

---
+++ src/IgnoreCaseToggle.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const IgnoreCaseToggle = ({ checked, onChange }) => {
+  return (
+    <>
+      <label htmlFor="ignore-case">Ignore case?</label>
+      <input
+        type="checkbox"
+        id="ignore-case"
+        checked={checked}
+        onChange={(e) => onChange(!checked)}
+      />
+    </>
+  );
+};
